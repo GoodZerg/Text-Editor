64e5c987874715594ee6e9dc1677ced93ffa64d9
[ sora. ] <57730505+GoodZerg@users.noreply.github.com>
2020-11-24 22:11:59 +0300

    офнул sfml подключил openGL и glad

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,38 @@
-#include <SFML/Graphics.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+int main() {
+  // Load GLFW and Create a Window
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  auto mWindow = glfwCreateWindow(500, 500, "OpenGL", nullptr, nullptr);
 
-int main(){
+  // Check for Valid Context
+  if (mWindow == nullptr) {
+    return 0;
+  }
+
+  // Create Context and Load OpenGL Functions
+  glfwMakeContextCurrent(mWindow);
+  gladLoadGL();
+
+  // Rendering Loop
+  while (glfwWindowShouldClose(mWindow) == false) {
+    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(mWindow, true);
+
+    // Background Fill Color
+    glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Flip Buffers and Draw
+    glfwSwapBuffers(mWindow);
+    glfwPollEvents();
+  }   
+  glfwTerminate();
   return 0;
 }
